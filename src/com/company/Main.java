3c8b59cd6620a5d95d11cfe7996b2a691ddc29a8
zhiheng.he <zhiheng.he@thoughtworks.com>
2020-01-07 13:52:10 +0800

    3、 restore to original version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,15 @@
 package com.company;
 
 import com.company.utilities.args.Args;
-import com.company.utilities.args.ArgsException;
 
 public class Main {
 
     public static void main(String[] args) {
         try{
-            Args arg = new Args("l, p#, d*", args);
+            Args arg = new Args("l", args);
             boolean logging = arg.getBoolean('l');
-            int port = arg.getInt('p');
-            String directory = arg.getString('d');
-        }catch (ArgsException e){
-            System.out.printf("ArgumentError:%s\n", e.errorMessage());
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
--- src/com/company/utilities/args/Args.java
+++ src/com/company/utilities/args/Args.java
@@ -1,19 +1,12 @@
 package com.company.utilities.args;
 
 public class Args {
-    public Args(String schema, String[] args) throws ArgsException {
+
+    public Args(String schema, String[] args) {
 
     }
 
     public boolean getBoolean(char l) {
         return false;
-    }
-
-    public int getInt(char p) {
-        return 0;
-    }
-
-    public String getString(char d) {
-        return null;
     }
 }
