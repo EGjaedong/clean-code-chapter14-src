bc6f382d01c1d2d94d53b1bf95c0c7c25964f35c
zhiheng.he <WOshinibaba2>
2020-01-10 14:54:36 +0800

    add test

---
+++ test/com/company/utilities/getopts/ArgsTest.java
@@ -0,0 +1,12 @@
+package com.company.utilities.getopts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgsTest {
+    @Test
+    void test1() {
+        assertTrue(true);
+    }
+}
