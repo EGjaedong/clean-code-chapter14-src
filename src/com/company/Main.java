0481a14b729a75aee37a29edbf385aed5dee9336
zhiheng.he <WOshinibaba2>
2020-01-15 13:39:08 +0800

    21、 complete Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,23 @@
 package com.company;
 
 import com.company.utilities.getopts.Args;
+import com.company.utilities.getopts.ArgsException;
 
+/**
+ * 运行命令： -l -p 123 -d abc
+ * 输出结果： true:123:abc
+ */
 public class Main {
 
     public static void main(String[] args) {
         try{
             Args arg = new Args("l, p#, d*", args);
             boolean logging = arg.getBoolean('l');
-        }catch (Exception e){
-            e.printStackTrace();
+            int port = arg.getInt('p');
+            String directory = arg.getString('d');
+            System.out.println(logging + ":" + port + ":" + directory);
+        }catch (ArgsException e){
+            System.out.printf("ArgumentError:%s\n", e.errorMessage());
         }
     }
 }
